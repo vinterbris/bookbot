0dafe8c3710c7670045cd175b89730e0fd5da90e
Sergey Dobrovolskiy <21102027+vinterbris@users.noreply.github.com>
2024-09-03 10:31:44 +0300

    fixed readability

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+path = 'books/frankenstein.txt'
+
 def main():
-    path = 'books/frankenstein.txt'
     text = get_book_text(path)
     number_of_words = get_number_of_words(text)
     chars_dict = get_chars_dict(text)
@@ -9,17 +10,7 @@ def main():
 
     print_how_many_alphas_were_found(chars_dict)
     print('--- End report ---')
-    
-def sort_on(tpl):
-    return tpl[1]
 
-def print_how_many_alphas_were_found(chars_dict):
-    list_from_dict = list(chars_dict.items())
-
-    list_from_dict.sort(reverse=True, key=sort_on)
-    for i in list_from_dict:
-        if i[0].isalpha():
-            print(f'The {i[0]} character was found {i[1]} times')
 
 def get_book_text(path):
     with open(path) as file:
@@ -28,6 +19,7 @@ def get_book_text(path):
 
 def get_number_of_words(text):
     return len(text.split())
+
 
 def get_chars_dict(text):
     num_of_chars = {}
@@ -38,5 +30,19 @@ def get_chars_dict(text):
         else:
             num_of_chars[char] += 1
     return num_of_chars
+
+
+def sort_on(pair_char_amount):
+    return pair_char_amount[1]
+
+
+def print_how_many_alphas_were_found(chars_dict):
+    list_from_dict = list(chars_dict.items())
+
+    list_from_dict.sort(reverse=True, key=sort_on)
+    for pair in list_from_dict:
+        if pair[0].isalpha():
+            print(f'The {pair[0]} character was found {pair[1]} times')
+
 
 main()
